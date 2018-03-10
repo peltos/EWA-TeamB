905c5fc59f2b367d7d59c791360060019d3e29b7
R. Abdelhadi <17270078+Rachid-ab@users.noreply.github.com>
2018-03-10 21:59:28 +0100

    Timeline template + stylesheet & minor header changes.

--- application/view/home/events.php
+++ application/view/home/events.php
@@ -1,4 +1,58 @@
-<div class="container">
-    <h2>You are in the View: application/view/home/events.php (everything in the box comes from this file)</h2>
-    <p>In a real application this could be a normal page.</p>
-</div>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+      <meta charset="utf-8">
+      <title>Ziggo</title>
+      <meta name="description" content="">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link href="<?php echo URL; ?>css/timeline.css" rel="stylesheet">
+    </head>
+
+    <body class="light-theme">
+      <ul class="timeline">
+
+        <!-- Item 1 -->
+        <li>
+          <div class="direction-r">
+            <div class="flag-wrapper">
+              <span class="hexa"></span>
+              <span class="flag">World of Warcraft - 2018 Spring Season Finals</span>
+              <span class="time-wrapper"><span class="time">Feb 2015</span></span>
+            </div>
+            <div class="desc">
+              The Spring Season Finals will feature the teams that has acquired the most amount of AWC Points
+              in both the EU cups and the NA cups. This is one of two Season Finals this year before BlizzCon.
+              The other one will be held in the Summer this year.
+              The teams participating will not only be playing for a $100,000 total prize pool, they will also play
+              for a direct invited to the 2018 Arena World Championship.
+              The tournament will be held Offline and is set to be held some time in the Middle or end of March.
+            </div>
+          </div>
+        </li>
+
+        <!-- Item 2 -->
+        <li>
+          <div class="direction-l">
+            <div class="flag-wrapper">
+              <span class="hexa"></span>
+              <span class="flag">Lorem ipsum Anim.</span>
+              <span class="time-wrapper"><span class="time">Dec 2014</span></span>
+            </div>
+            <div class="desc">Lorem ipsum In ut sit in dolor nisi ex magna eu anim anim tempor dolore aliquip enim cupidatat laborum dolore.</div>
+          </div>
+        </li>
+
+        <!-- Item 3 -->
+        <li>
+          <div class="direction-r">
+            <div class="flag-wrapper">
+              <span class="hexa"></span>
+              <span class="flag">Lorem Occaecat.</span>
+              <span class="time-wrapper"><span class="time">July 2014</span></span>
+            </div>
+            <div class="desc">Lorem ipsum Minim labore Ut cupidatat quis qui deserunt proident fugiat pariatur cillum cupidatat reprehenderit sit id dolor consectetur ut.</div>
+          </div>
+        </li>
+      </ul>
+    </body>
+</html>
