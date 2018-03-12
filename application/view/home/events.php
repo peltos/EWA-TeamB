5871b9672cc6c1645d2f9ab07999d410a364e2a6
R. Abdelhadi <17270078+Rachid-ab@users.noreply.github.com>
2018-03-12 23:16:35 +0100

    Timeline stylesheet changes + PHP API variables added.

--- application/view/home/events.php
+++ application/view/home/events.php
@@ -1,5 +1,7 @@
 <div class="container timeline-container" data-url="<?php if (!empty($url)) echo $url ?>">
 
+  <h2>Tournament Events</h2>
+
     <ul class="timeline">
         <?php
         $counter = 0;
@@ -15,9 +17,12 @@
                         echo '<div class="flag-wrapper">';
                             echo '<span class="hexa"></span>';
                             echo '<span class="flag">' . $item["name"] . '</span>';
-                            echo '<span class="time-wrapper"><span class="time">' . $item["begin_at"] . '</span>';
+                            echo '<span class="time-wrapper"><span class="time"><b>' . date('d F H:i', strtotime($item["begin_at"])) . '</b></span>';
                         echo '</div>';
                         echo '<div class="desc">' .
+                                '<span class="game">' . $item["videogame"]['name'] . '</span>',
+                                '<span class="rounds">' . $item["number_of_games"] . '</span>',
+                                '<span class="season">' . $item["tournament"]['name'] . '</span>',
                                 '<img src="' .$item["league"]["image_url"] .
                              '"/></div>' ;
                     echo '</div>';
