6c0842d2cc2dfe8021418362020f500f26d89c62
Berna <berna.turker@hva.nl>
2018-03-13 14:41:14 +0100

    Streaming api fix

--- application/view/home/streamers.php
+++ application/view/home/streamers.php
@@ -1,20 +1,69 @@
 <div class="container streamers">
   <h2>streamers</h2>
+  <h2>FIFA 18</h2>
+  <ul class="streamers__list">
 
-  <ul class="timeline">
-      <?php
-      if (!empty($streamers)) {
-          foreach ($streamers as $key => $item) {
-            // if($item["type"]["name"] == "League of Legends"){
-              echo $item["id"] . '</br>';
-              echo $item["type"]["name"] . '</br>';
-              echo $item["name"] . '</br>';
-              echo $item["audience"] . '</br>';
-              echo '<img src="' . $item["type"]["coverUrl"] . '" </br>';
+    <?php
+
+    if (!empty($streamers)) {
+        foreach ($streamers as $key => $item) {
+           if($item["type"]["name"] == "FIFA 18"){
+              echo '<a class="streamers--item" href="https://mixer.com/'.$item["token"].'" target="_blank">';
+              echo '<div class="streamers--item__container">';
+              echo '<img src="' . $item["type"]["coverUrl"] . '"/>';
+              echo '<p>' . $item["id"] . ' </p>';
+              echo '<p>' . $item["type"]["name"] . ' </p>';
+              echo '<p>' . $item["name"] . ' </p>';
+              echo '<p>' . $item["audience"] . ' </p>';
               echo '</br>';
-            // }
-          }
-      }
-      ?>
+            echo '</div>';
+          echo '</a>';
+         }
+        }
+    } ?>
+
+    </ul>
+    <h2>Battlefield 4</h2>
+    <ul class="streamers__list">
+
+    <?php if (!empty($streamers)) {
+
+        foreach ($streamers as $key => $item) {
+           if($item["type"]["name"] == "Battlefield 4"){
+              echo '<a class="streamers--item" href="https://mixer.com/'.$item["token"].'" target="_blank">';
+              echo '<div class="streamers--item__container">';
+              echo '<img src="' . $item["type"]["coverUrl"] . '"/>';
+              echo '<p>' . $item["id"] . ' </p>';
+              echo '<p>' . $item["type"]["name"] . ' </p>';
+              echo '<p>' . $item["name"] . ' </p>';
+              echo '<p>' . $item["audience"] . ' </p>';
+              echo '</br>';
+            echo '</div>';
+          echo '</a>';
+        }
+        }
+    }?>
+
+    </ul>
+    <h2>Fortnite </h2>
+    <ul class="streamers__list">
+
+    <?php if (!empty($streamers)) {
+        foreach ($streamers as $key => $item) {
+           if($item["type"]["name"] == "Fortnite"){
+              echo '<a class="streamers--item" href="https://mixer.com/'.$item["token"].'" target="_blank">';
+              echo '<div class="streamers--item__container">';
+              echo '<img src="' . $item["type"]["coverUrl"] . '"/>';
+              echo '<p>' . $item["id"] . ' </p>';
+              echo '<p>' . $item["type"]["name"] . ' </p>';
+              echo '<p>' . $item["name"] . ' </p>';
+              echo '<p>' . $item["audience"] . ' </p>';
+              echo '</br>';
+            echo '</div>';
+          echo '</a>';
+        }
+        }
+    }
+    ?>
   </ul>
 </div>
