1d3b7a228f1099bfffabf5cc76f1eec4ce43623c
R.J. Pelt <ron.pelt@hva.nl>
2018-03-19 22:57:53 +0100

    improved route ajax

--- application/view/_templates/footer.php
+++ application/view/_templates/footer.php
@@ -23,6 +23,9 @@
     <!-- our JavaScript -->
     <script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+        <script>
+            let URL = '<?php echo URL; ?>';
+        </script>
     <script src="<?php echo URL; ?>js/application.js"></script>
     <script src="<?php echo URL; ?>js/streamers.js"></script>
     <script src="<?php echo URL; ?>js/nightmode.js"></script>
--- public/js/nightmode.js
+++ public/js/nightmode.js
@@ -1,16 +1,16 @@
 $( document ).ready(function() {
     let nightmode = $("#nightmode");
     let bodyClass = $("body");
-    let url = window.location.href;
 
     nightmode.click(function(){
         bodyClass.toggleClass("light-theme dark-theme");
         let bodyVar = bodyClass.attr('class');
         $.ajax({
-            url: '/EWA-TeamB/ajax/ajaxnightmode',
+            url: URL + 'ajax/ajaxnightmode',
             type: "post",
             data: "modus=" + bodyVar,
             success: function (html) {
+                alert(URL + 'ajax/ajaxnightmode');
             }
         });
 
