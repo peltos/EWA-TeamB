953219fcc7a4262487a145df4fac9326ffe90475
R.J. Pelt <ron.pelt@hva.nl>
2018-03-19 14:25:03 +0100

    show active in nav

--- application/view/tournaments/eventsheader.php
+++ application/view/tournaments/eventsheader.php
@@ -1,5 +1,6 @@
 <?php
 $page = 'timeline';
+$active = '//' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
 ?>
 <div class="container timeline-container" data-url="<?php if (!empty($url)) echo $url ?>">
 
@@ -7,12 +8,12 @@ $page = 'timeline';
         <div class="filterBar--inner">
             <div class="dateFilter">
                 <a class="dateFilter--item" href="<?php echo URL; ?>tournaments"><i class="filter-icon fas fa-filter"></i></a>
-                <a class="dateFilter--item" href="<?php echo URL; ?>tournaments">upcoming</a>
-                <a class="dateFilter--item" href="<?php echo URL; ?>tournaments/running">running</a>
-                <a class="dateFilter--item" href="<?php echo URL; ?>tournaments/past">past</a>
-                <a class="dateFilter--item" href="<?php echo URL; ?>tournaments/lol">League of Legends</a>
-                <a class="dateFilter--item" href="<?php echo URL; ?>tournaments/dota2">Dota 2</a>
-                <a class="dateFilter--item" href="<?php echo URL; ?>tournaments/ow">Overwatch</a>
+                <a class="dateFilter--item <?php if($active == (URL . 'tournaments')) {echo 'active';} ?>" href="<?php echo URL; ?>tournaments">upcoming</a>
+                <a class="dateFilter--item <?php if($active == (URL . 'tournaments/running')) {echo 'active';} ?>" href="<?php echo URL; ?>tournaments/running">running</a>
+                <a class="dateFilter--item <?php if($active == (URL . 'tournaments/past')) {echo 'active';} ?>" href="<?php echo URL; ?>tournaments/past">past</a>
+                <a class="dateFilter--item <?php if($active == (URL . 'tournaments/lol')) {echo 'active';} ?>" href="<?php echo URL; ?>tournaments/lol">League of Legends</a>
+                <a class="dateFilter--item <?php if($active == (URL . 'tournaments/dota2')) {echo 'active';} ?>" href="<?php echo URL; ?>tournaments/dota2">Dota 2</a>
+                <a class="dateFilter--item <?php if($active == (URL . 'tournaments/ow')) {echo 'active';} ?>" href="<?php echo URL; ?>tournaments/ow">Overwatch</a>
             </div>
         </div>
     </div>
