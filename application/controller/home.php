5de16133bf46c4bf9b5958d80825c7deb4812b9f
R.J. Pelt <ron.pelt@hva.nl>
2018-04-07 13:11:19 +0200

    favorites on the front page

--- application/controller/home.php
+++ application/controller/home.php
@@ -17,9 +17,14 @@ class Home extends Controller
     public function index()
     {
         $slider = $this->model->timeline();
+        $favorites = $this->model->getFavorites('pelt8@hotmail.com');
+        $favoritePage = $this->model->getFavoritePage($favorites);
+
         // load views
         require APP . 'view/_templates/header.php';
         require APP . 'view/home/index.php';
         require APP . 'view/_templates/footer.php';
+
+        $this->model->streamerUpdate('mixer', $favoritePage);
     }
 }
