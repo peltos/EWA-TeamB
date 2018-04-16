cdc963068719622ea26b6d1c961be080539c394d
Berna <berna.turker@hva.nl>
2018-04-16 15:23:35 +0200

    Two news sources

---
+++ application/view/news/index.php
@@ -0,0 +1,47 @@
+<h1>Esport news feed</h1>
+<!-- start sw-rss-feed code -->
+<script type="text/javascript">
+<!--
+
+rssfeed_url = new Array();
+rssfeed_url[0]="https://www.idlethumbs.net/feeds/esportstoday"; rssfeed_url[1]="https://news.google.com/news/rss/search/section/q/esport/esport?hl=en&gl=US&ned=us";
+rssfeed_frame_width="1000";
+rssfeed_frame_height="1000";
+rssfeed_scroll="off";
+rssfeed_scroll_step="6";
+rssfeed_scroll_bar="on";
+rssfeed_target="_blank";
+rssfeed_font_size="12";
+rssfeed_font_face="";
+rssfeed_border="on";
+rssfeed_css_url="https://feed.surfing-waves.com/css/style4.css";
+rssfeed_title="on";
+rssfeed_title_name="";
+rssfeed_title_bgcolor="#3366ff";
+rssfeed_title_color="#fff";
+rssfeed_title_bgimage="";
+rssfeed_footer="off";
+rssfeed_footer_name="rss feed";
+rssfeed_footer_bgcolor="#fff";
+rssfeed_footer_color="#333";
+rssfeed_footer_bgimage="";
+rssfeed_item_title_length="50";
+rssfeed_item_title_color="#666";
+rssfeed_item_bgcolor="#fff";
+rssfeed_item_bgimage="";
+rssfeed_item_border_bottom="on";
+rssfeed_item_source_icon="off";
+rssfeed_item_date="off";
+rssfeed_item_description="on";
+rssfeed_item_description_length="120";
+rssfeed_item_description_color="#666";
+rssfeed_item_description_link_color="#333";
+rssfeed_item_description_tag="on";
+rssfeed_no_items="0";
+rssfeed_cache = "fc503d00e021cab80551c5131c1f324f";
+//-->
+</script>
+<script type="text/javascript" src="//feed.surfing-waves.com/js/rss-feed.js"></script>
+<!-- The link below helps keep this service FREE, and helps other people find the SW widget. Please be cool and keep it! Thanks. -->
+<div style="text-align:right; width:1000px;">powered by <a href="http://www.surfing-waves.com" rel="noopener" target="_blank" style="color:#ccc;font-size:10px">Surfing Waves</a></div>
+<!-- end sw-rss-feed code -->
