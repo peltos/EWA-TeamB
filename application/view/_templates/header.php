587edf32d6bc5e05d11bf36e49c20243982e392d
R.J. Pelt <ron.pelt@hva.nl>
2018-04-23 16:33:04 +0200

    nav menu style changed

--- application/view/_templates/header.php
+++ application/view/_templates/header.php
@@ -59,7 +59,7 @@
                         <a class="navigation--item menu-box" title="Sign In" href="<?php echo URL; ?>signin"><i class="fas fa-sign-in-alt"></i></a>
                     <?php } else{ ?>
                         <a class="navigation--item" href="<?php echo URL; ?>favorite">Favorites</a>
-                        <a class="navigation--item menu-box" href="<?php echo URL; ?>home/logout">Sign Out</i></a>
+                        <a class="navigation--item menu-box" href="<?php echo URL; ?>home/logout"><i class="fas fa-power-off"></i></a>
                     <?php } ?>
 
                 </div>
