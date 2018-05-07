b7aef0571d751aabaedf48a8c8c3d9411a41fdb8
R. Abdelhadi <17270078+Rachid-ab@users.noreply.github.com>
2018-05-07 15:10:37 +0200

    Created Profile page + Menu Icon.

---
+++ application/controller/profile.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Class Profile
+ *
+ * Please note:
+ * Don't use the same name for class and method, as this might trigger an (unintended) __construct of the class.
+ * This is really weird behaviour, but documented here: http://php.net/manual/en/language.oop5.decon.php
+ *
+ */
+class Profile extends Controller
+{
+    /**
+     * PAGE: index
+     * This method handles what happens when you move to http://yourproject/profile/index (which is the default page btw)
+     */
+    public function index()
+    {
+        // load views
+        require APP . 'view/_templates/header.php';
+        require APP . 'view/profile/index.php';
+        require APP . 'view/_templates/footer.php';
+    }
+}
