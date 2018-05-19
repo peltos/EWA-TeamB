f33fd9a7e4a4a72ca80ef90a69d83f6d441891ef
R. Abdelhadi <17270078+Rachid-ab@users.noreply.github.com>
2018-05-19 16:07:13 +0200

    Added Player Filter option

--- application/controller/players.php
+++ application/controller/players.php
@@ -21,4 +21,46 @@ class Players extends Controller
         require APP . 'view/players/index.php';
         require APP . 'view/_templates/footer.php';
     }
+
+    /**
+     * PAGE: league of legends Players
+     * This method handles what happens when you move to http://yourproject/matches/lol
+     */
+    public function lol()
+    {
+        $slider = $this->model->getPlayers();
+
+        // load views
+        require APP . 'view/_templates/header.php';
+        require APP . 'view/players/index.php';
+        require APP . 'view/_templates/footer.php';
+    }
+
+    /**
+     * PAGE: Dota 2 Players
+     * This method handles what happens when you move to http://yourproject/matches/dota2
+     */
+    public function dota2()
+    {
+        $slider = $this->model->getPlayers();
+
+        // load views
+        require APP . 'view/_templates/header.php';
+        require APP . 'view/players/index.php';
+        require APP . 'view/_templates/footer.php';
+    }
+
+    /**
+     * PAGE: Overwatch Players
+     * This method handles what happens when you move to http://yourproject/matches/ow
+     */
+    public function ow()
+    {
+        $slider = $this->model->getPlayers();
+
+        // load views
+        require APP . 'view/_templates/header.php';
+        require APP . 'view/players/index.php';
+        require APP . 'view/_templates/footer.php';
+    }
 }
--- application/view/players/index.php
+++ application/view/players/index.php
@@ -1,6 +1,6 @@
-<div class="container players-container">
+<?php require APP . 'view/players/playerfilter.php'; ?>
 
-  <h2> Players </h2>
+<div class="container players-container">
 
     <ul class="slider">
         <?php
@@ -10,17 +10,38 @@
               if ($item["first_name"] != null && $item["image_url"] != null) { ?>
                 <li class="slider-item">
                     <span class="slider-item--titles">
-                        <p class="slider-item--content__league"><?php echo $item['name'] ?></p>
-                        <p class="slider-item--content__tournament"><?php echo $item["first_name"] ?> <?php echo $item["last_name"] ?></p>
+                        <p class="slider-item--content__nickname"><?php echo $item['name'] ?></p>
+                        <p class="slider-item--content__firstLastname"><?php echo $item["first_name"] ?> <?php echo $item["last_name"] ?></p>
                     </span>
                     <div class="slider-item--icon">
                         <img class="slider-item--icon__image" src=" <?php echo $item["image_url"] ?> "/>
                     </div>
                     <div class="slider-item--content">
-                      <p class="slider-item--content__name"><?php echo $item["current_team"]["name"] ?></p>
-                      <p class="slider-item--content__date"><i class="fas fa-map-marker-alt"></i> <?php echo $item["hometown"] ?></p>
+                      <p class="slider-item--content__team">
+                        <?php if ($item["current_team"]["name"] != null) { ?>
+                          <?php echo $item["current_team"]["name"] ?></p>
+                        <?php } else { ?>
+                                - No Team - </p>
+                        <?php } ?>
+                      <p class="slider-item--content__hometown">
+                        <i class="fas fa-map-marker-alt"></i>
+                        <?php if ($item["hometown"] != null) { ?>
+                          <?php echo $item["hometown"] ?></p>
+                        <?php } else { ?>
+                                Unkown </p>
+                        <?php } ?>
+                      <p class="slider-item--content__game">
+                        <i class="fas fa-trophy"></i>
+                        <?php if ($item["current_videogame"]["name"] != null) { ?>
+                            <?php if ($item["current_videogame"]["name"] != 'LoL') { ?>
+                              <?php echo $item["current_videogame"]["name"] ?></p>
+                            <?php } else { ?>
+                                    League of Legends
+                            <?php } ?>
+                        <?php } else { ?>
+                                Unkown </p>
+                        <?php } ?>
                     </div>
-
                 </li>
                 <?php
                 $break++;
