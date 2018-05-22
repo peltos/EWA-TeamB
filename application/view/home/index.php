eca7ea8c8aab4a061aa057b91a875981ac3bd1a8
R.J. Pelt <pelt8@hotmail.com>
2018-05-22 13:33:48 +0200

    added view more buttons on homepage

--- application/view/home/index.php
+++ application/view/home/index.php
@@ -1,62 +1,62 @@
 <div class="container homepage-container">
-    <h2 class="homepage-title">Recommended Streams</h2>
+
+    <h2 class="homepage-title"><a href="streams">Recommended Streams</a></h2>
     <ul class="streamers__list">
+        <?php
+        $counter = 0;
+        if (!empty($favoritePageRecommendedMixer)) {
+            foreach ($favoritePageRecommendedMixer as $key => $item) {
+                if ($item["online"] == true) {
+                    if ($counter < 4) {
+                        include APP . 'view/_templates/streamer-mixer.php';
+                    }
+                }
+            }
+        }
 
-        <?php  ?>
+        if (!empty($favoritePageRecommendedTwitch)) {
+            foreach ($favoritePageRecommendedTwitch as $key => $item) {
+                if (!$item['stream'] == null) {
+                    if ($counter < 4) {
+                        include APP . 'view/_templates/streamer-twitch.php';
+                    }
+                }
+            }
+        }
+        ?>
+    </ul>
+    <div class="view-more__container">
+        <a class="view-more btn" href="streams">View More...</a>
+    </div>
 
-
-                <?php
-                $counter = 0;
-                if (!empty($favoritePageRecommendedMixer)) {
-                    foreach ($favoritePageRecommendedMixer as $key => $item) {
-                        if ($item["online"] == true) {
-                            if ($counter < 4) {
-                                include APP . 'view/_templates/streamer-mixer.php';
-                            }
+    <?php if (!$_SESSION['email'] == '') { ?>
+        <h2 class="homepage-title"><a href="favorite">Followed Channels</a></h2>
+        <ul class="streamers__list">
+            <?php
+            $counter = 0;
+            if (!empty($favoritePage)) {
+                foreach ($favoritePage as $key => $item) {
+                    if ($item["online"] == true) {
+                        if ($counter < 4) {
+                            include APP . 'view/_templates/streamer-mixer.php';
                         }
                     }
                 }
+            }
 
-                if (!empty($favoritePageRecommendedTwitch)) {
-                    foreach ($favoritePageRecommendedTwitch as $key => $item) {
-                        if (!$item['stream'] == null) {
-                            if ($counter < 4) {
-                                include APP . 'view/_templates/streamer-twitch.php';
-                            }
+            if (!empty($favoritePageTwitch)) {
+                foreach ($favoritePageTwitch as $key => $item) {
+                    if (!$item['stream'] == null) {
+                        if ($counter < 4) {
+                            include APP . 'view/_templates/streamer-twitch.php';
                         }
                     }
                 }
-                ?>
-            </ul>
-
-        <?php  ?>
-        <?php if (!$_SESSION['email'] == '') { ?>
-
-            <h2 class="homepage-title">Followed Channels</h2>
-            <ul class="streamers__list">
-                <?php
-                $counter = 0;
-                if (!empty($favoritePage)) {
-                    foreach ($favoritePage as $key => $item) {
-                        if ($item["online"] == true) {
-                            if ($counter < 4) {
-                                include APP . 'view/_templates/streamer-mixer.php';
-                            }
-                        }
-                    }
-                }
-
-                if (!empty($favoritePageTwitch)) {
-                    foreach ($favoritePageTwitch as $key => $item) {
-                        if (!$item['stream'] == null) {
-                            if ($counter < 4) {
-                                include APP . 'view/_templates/streamer-twitch.php';
-                            }
-                        }
-                    }
-                }
-                ?>
-            </ul>
-
-        <?php } ?>
+            }
+            ?>
+        </ul>
+        <div class="view-more__container">
+            <a class="view-more btn" href="favorite">View More...</a>
+        </div>
+    <?php } ?>
 </div>
