2f3c482319ce6b5ddc7453748d45424c99d4024b
R.J. Pelt <pelt8@hotmail.com>
2018-06-04 17:51:49 +0200

    fixed tournaments js

--- public/js/tournaments.js
+++ public/js/tournaments.js
@@ -1,19 +1,20 @@
 $( document ).ready(function() {
 
     // show the team screen when clicked on the team icon
-
+    let itemTeams = $(".slider-item--teams");
     let switchIconsTeams = $(".switch-icons");
 
-    item.click(function(){
+    itemTeams.click(function(){
         $(this).parent(switchIcons).next().toggleClass("teams");
         $(this).parent(switchIcons).next().next().removeClass("match");
     });
 
     // show the tournaments screen when clicked on the tournaments icon
 
+    let itemTournaments = $(".slider-item--info");
     let switchIconsTournaments = $(".switch-icons");
 
-    item.click(function(){
+    itemTournaments.click(function(){
         $(this).parent(switchIcons).next().next().toggleClass("match");
         $(this).parent(switchIcons).next().removeClass("teams");
     });
