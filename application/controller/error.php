36f014e0b0fe30e1003058c0581e680d15ec5cb8
R.J. Pelt <pelt8@hotmail.com>
2018-06-04 14:57:40 +0200

    changed error/index.php too error.php

--- application/controller/error.php
+++ application/controller/error.php
@@ -20,7 +20,7 @@ class Error extends Controller
     {
         // load views
         require APP . 'view/_templates/header.php';
-        require APP . 'view/error/index.php';
+        require APP . 'view/error/error.php';
         require APP . 'view/_templates/footer.php';
     }
 }
